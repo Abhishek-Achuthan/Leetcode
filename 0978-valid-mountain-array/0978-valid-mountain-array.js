daba69b9df9ee7e1222070c7b68018bf3f668279
Abhishek MA <abhishekachuthan693@gmail.com>
2025-11-17 20:52:41 +0530

    Time: 49 ms (70.86%), Space: 58.7 MB (27.27%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.js
+++ 0978-valid-mountain-array/0978-valid-mountain-array.js
@@ -3,23 +3,19 @@
  * @return {boolean}
  */
 var validMountainArray = function(arr) {
-    if(arr.length<3) return false;
-    let lg = arr.length
-    let max = Math.max(...arr);
-    let maxIndex = arr.indexOf(max)
-    if(maxIndex===0 || maxIndex===lg-1)return false;
-    if(max===arr[arr.length-1]) return false
-    let answer = true;
-    for(let i = 0 ; i<lg ; i++) {
-        if(i < maxIndex) {
-            if(arr[i+1]<=arr[i]) {
-                answer = false;
+    if(arr.length<3)return false;
+    if(arr[0]>=arr[1] || arr[arr.length-1]>=arr[arr.length-2])return false;
+    let inc = true;
+    for(let i=0;i<arr.length;i++){
+        if(inc){
+            if(arr[i+1]<=arr[i]){
+                inc = false
             }
         }else{
-            if(arr[i+1]>=arr[i]) {
-                answer = false;
+            if(arr[i+1]>=arr[i]){
+                return false;
             }
         }
     }
-    return answer;
+    return true;
 };
