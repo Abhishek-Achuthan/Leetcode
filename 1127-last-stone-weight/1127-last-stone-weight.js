84d9bcedb912f6019a4acd0cfc7a0ea3a54ddbf7
Abhishek MA <abhishekachuthan693@gmail.com>
2025-11-18 11:24:36 +0530

    Time: 2 ms (73.87%), Space: 54.5 MB (92.34%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.js
+++ 1127-last-stone-weight/1127-last-stone-weight.js
@@ -3,17 +3,24 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
+    
     if(stones.length === 1) return stones[0];
-    let sorted = stones.sort((a,b) => a-b);
-    while(sorted.length>1) {
-        let temp1 = sorted.pop();
-        let temp2 = sorted.pop();
-        if(temp1!==temp2) {
-            sorted.push(temp1-temp2);
+
+    let sorted = stones.sort((a,b) => b-a);
+
+    for(let i = 0; i<sorted.length-1 ; i++) {
+        if(sorted[i] === sorted[i+1]) {
+            sorted.splice(i,2);
+            i--;
+        }else if(sorted[i] != sorted[i+1]) {
+            let val = sorted[i]-sorted[i+1]
+            sorted.push(val);
+            sorted.splice(i,2);
+            sorted.sort((a,b) => b-a)
+            i--;
         }
-        sorted = sorted.sort((a,b) => a-b);
     }
-    return sorted[0]??0
+    return sorted[0]?? 0;
 
 
 
