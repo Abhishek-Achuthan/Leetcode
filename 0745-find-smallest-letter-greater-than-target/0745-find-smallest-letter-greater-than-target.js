7ff732ca800095bcbf6cbef7831f8981930e6f36
Abhishek MA <abhishekachuthan693@gmail.com>
2025-11-20 10:46:59 +0530

    Time: 5 ms (3.85%), Space: 58.2 MB (6.11%) - LeetHub

---
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+   let targetCharCode = target.charCodeAt(0);
+   
+   for(let i =0; i<letters.length ; i++) {
+    if(letters[i].charCodeAt(0)>targetCharCode) {
+       console.log(letters[i].charCodeAt(0))
+        return letters[i]
+    }
+   }
+   return letters[0]
+};
