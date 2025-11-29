17f4976454f2c58cff4f8e620ecb0a6d0c09ff8b
Abhishek MA <abhishekachuthan693@gmail.com>
2025-11-29 10:16:09 +0530

    Time: 8 ms (53.85%), Space: 60.4 MB (55.98%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -2,20 +2,26 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function (s) {
+var reverseVowels = function(s) {
     let vowels = new Set('aeiouAEIOU');
     let arr = s.split('');
     let left = 0;
-    let right = s.length - 1;
-
-    while (left < right) {
-        if (!vowels.has(arr[left])) { left++; continue; }
-        if (!vowels.has(arr[right])) { right--; continue; }
-        const tmp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = tmp;
-        left++;
-        right--;
+    let right = s.length-1;
+    while(left<right) {
+        if(vowels.has(s[left])&& vowels.has(arr[right])) {
+            let temp =0;
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--
+        }
+        if(!vowels.has(arr[left])) {
+            left++;
+        }
+         if(!vowels.has(arr[right])) {
+            right--;
+        }
     }
     return arr.join('');
 };
