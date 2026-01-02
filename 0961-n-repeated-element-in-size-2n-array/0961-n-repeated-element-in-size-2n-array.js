cde6bc9957046e9a16effe30aa13773ef3e960eb
Abhishek MA <abhishekachuthan693@gmail.com>
2026-01-02 11:35:08 +0530

    Time: 0 ms (100%), Space: 56.9 MB (73.95%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+    let map = {};
+
+    for(let num of nums) {
+        map[num] = (map[num] || 0) +1;
+
+        if(map[num] ===2) {
+            return num
+        }
+    }
+
+  
+    
+    };
