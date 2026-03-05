ae6e5a7b25b2063e614aeba8af4a3ddc28210fb5
Abhishek MA <abhishekachuthan693@gmail.com>
2026-03-05 13:05:08 +0530

    Time: 0 ms (100%), Space: 54.7 MB (91.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -2,32 +2,30 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
+var trap = function (height) {
     let left = 0;
-    let right = height.length-1;
+    let right = height.length - 1;
 
-    let maxLeft = height[left];
-    let maxRight = height[right];
+    let maxLeft = 0
+    let maxRight = 0
 
     let water = 0;
 
-    while(left < right) {
-        if(height[left] < height[right]) {
-            left ++;
-            if(height[left] > maxLeft) {
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= maxLeft) {
                 maxLeft = height[left];
-            };
-            if(height[left] < maxLeft || height[left] < maxRight) {
-                water += Math.min(maxLeft,maxRight) - height[left];
+            } else {
+                water += maxLeft - height[left];
             }
-        }else {
-            right --;
-            if(height[right] > maxRight) {
+            left++
+        } else {
+            if (height[right] > maxRight) {
                 maxRight = height[right];
-            };
-            if(height[right] < maxRight || height[right] < maxRight) {
-                water += Math.min(maxLeft,maxRight) - height[right];
+            } else {
+                water += maxRight - height[right];
             }
+            right--;
         }
     }
 
