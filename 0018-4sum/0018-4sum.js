8a5750ea19f5e4e08ebb3289e52c549217710cb9
Abhishek MA <abhishekachuthan693@gmail.com>
2026-03-05 17:16:57 +0530

    Time: 14 ms (78.97%), Space: 59.1 MB (24.58%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+
+    nums.sort((a, b) => a - b);
+
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i - 1] === nums[i]) {
+            continue;
+        }
+        for (let j = i + 1; j < nums.length; j++) {
+            if ( j > i + 1 &&nums[j - 1] === nums[j]) {
+                continue;
+            }
+            let left = j + 1
+            let right = nums.length - 1;
+            while (left < right) {
+                let value = nums[i] + nums[j] + nums[left] + nums[right]
+                if (value === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+                    left++;
+                    right--;
+                    while (left < right && nums[left - 1] == nums[left]) {
+                        left++;
+                    }
+                    while (right < nums.length - 1 && nums[right] == nums[right + 1] && left < right) {
+                        right--;
+                    }
+                } else if (value < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+    return result
+};
