df622324e6ecec5c68928241d9aeae691531331b
Abhishek MA <abhishekachuthan693@gmail.com>
2026-03-07 11:43:26 +0530

    Time: 12 ms (78.21%), Space: 58 MB (5.22%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -7,14 +7,9 @@ var threeSumClosest = function(nums, target) {
     nums.sort((a,b) => a-b);
     let closest = 0;
     let diff = Infinity;
-
     for(let i =0 ; i< nums.length-2 ; i++) {
-        if(i>0 && nums[i-1] === nums[i]){
-            continue;
-        }
         let left = i+1;
         let right = nums.length-1;
-
         while(left < right) {
             let sum = nums[i] + nums[left] + nums[right];
             let abs = Math.abs(sum - target);
@@ -27,7 +22,6 @@ var threeSumClosest = function(nums, target) {
             }else {
                 left++;
             }
-            
         }
     }
     return closest
