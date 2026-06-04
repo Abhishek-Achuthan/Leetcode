833f32f7db5f5ab59c2b208532f4c3c4bd3c19a1
Abhishek MA <abhishekachuthan693@gmail.com>
2026-06-04 17:52:02 +0530

    Time: 4 ms (86.01%), Space: 69.3 MB (79.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,20 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-   let prefix = new Array(nums.length).fill(1);
-   let suffix = new Array(nums.length).fill(1);
-   let result = [];
+   let result = new Array(nums.length).fill(1);
 
    for(let i = 1; i< nums.length; i++) {
-      prefix[i] = prefix[i-1] * nums[i-1]
+      result[i] = result[i-1] * nums[i-1]
    }
    
+   let suffix = 1;
    for(let j = nums.length-2 ; j >=0 ; j--) {
-      suffix[j] = suffix[j+1] * nums[j+1]
-   }
-  
-   for(let i = 0; i< nums.length ; i++) {
-     result.push(prefix[i] * suffix[i])
+      suffix *= nums[j+1]
+      result[j] *= suffix
    }
 
    return result;
