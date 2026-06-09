6f4bef59d47ed5c85b638d3daa06a7d59eb592fa
Abhishek MA <abhishekachuthan693@gmail.com>
2026-06-09 15:26:22 +0530

    Time: 0 ms (100%), Space: 53.9 MB (26.78%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -4,33 +4,25 @@
  */
 var decodeString = function(s) {
     let stack =[];
-    let si = -1;
 
     function isDigit(num) {
         return num >='0' && num<='9'
     }
-
     for(let i = 0; i<s.length ; i++) {
         if(s[i] !== ']') {
             stack.push(s[i]);
-            si++;
         }else{
             let substring = '';
-            while(stack[si] !== '[') {
+            while(stack[stack.length-1] !== '[') {
                 substring = stack.pop() + substring;
-                si--
             }
             stack.pop();
-            si--
             let k = '';
-            while(stack.length >=0&& isDigit(stack[si])) {
+            while(stack.length>=0&& isDigit(stack[stack.length-1])) {
                 k= stack.pop() + k;
-                si--;
             }
-
             substring = substring.repeat(k);
             stack.push(substring);
-            si+=1
         }
     }
     return stack.join('')
